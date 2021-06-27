9ca45a2eb71da57cb4d363e91a15877e190a3751
Guelsan <cem@guelsan.com>
2021-06-27 14:30:53 +0200

    inserted skeleton simulation entry

---
+++ ciruit_simulations/simulation_entry.py
@@ -0,0 +1,11 @@
+from PySpice.Spice.Netlist import Circuit
+
+def getMaxSettlingTime(testCircuit : Circuit) :
+    ''' TODO: Docstring
+    '''
+
+    # Do simulation
+
+    # Get and return max settling time
+
+    a = 1

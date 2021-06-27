b4242ad3bf0ecbb504533e1a86cb03de19df4aa7
Guelsan <cem@guelsan.com>
2021-06-27 18:01:46 +0200

    inserted synthesizeCircuit into main loop

--- main_controller.py
+++ main_controller.py
@@ -2,22 +2,21 @@ import time
 
 import pandas as pd
 from tqdm import tqdm
+from PySpice.Spice.Netlist import Circuit
 
-
+import generation.functions as gen_func
 import generation.graph as graph
 from generation.device import device
 from generation.node import node
 from generation.nodeLink import nodeLink
-import generation.functions as gen_func
-
 
 # Settings-------------------------------------------------------------------------------
-G               = graph.graph_P4()
-nlConfigOffset  = 0
+G               = graph.graph_P3()  	    # Choose graph type 
+nlConfigOffset  = 0                         # Offset for nlConfig loop
 
 
 # Initialize-----------------------------------------------------------------------------
-node.check(node)
+node.check_link(node)
 
 allNodes        = node.allNodes
 allNodeLinks    = nodeLink.allNodeLinks
@@ -28,7 +27,8 @@ paramConfigs    = gen_func.getParameterConfigs(G.TL_count)              # type: 
 
 print(nlConfigs)
 
-# Iterate over nlConfigs and paramConfigs------------------------------------------------
+# Iterate over nlConfigs and paramConfigs--------------------------------------
+# ----------
 for indx, nlConfig in tqdm( nlConfigs[nlConfigOffset:].iterrows(), position=0, ncols=70, 
                             total=nlConfigs.shape[0] - 1 - nlConfigOffset, desc='nlConfig    ') :
    
@@ -36,10 +36,17 @@ for indx, nlConfig in tqdm( nlConfigs[nlConfigOffset:].iterrows(), position=0, n
     nodeLink.configure(nodeLink, nlConfig)
         
     gen_func.synthesizeTopology(allNodeLinks, allDevices)
-    device.checkDevices(device)
+    device.check(device)
 
     for jndx, paramConfig in tqdm(paramConfigs.iterrows(), position=1, ncols=70,
                                   total=paramConfigs.shape[0] - 1, leave=False, desc='paramConfig ') :
+
+        # Circuit synth
+        circName        = f"{G.name}_nlC{indx:03}_pC{jndx:03}"
+        currCircuit     = gen_func.synthesizeCircuit(circName, G, paramConfig)
+
+        simulator       = currCircuit.simulator(temperature=25, nominal_temperature=25)
+        analysis        = simulator.transient(step_time=1e-11, end_time=100e-9)
 
         time.sleep(0.01)
         a = 1
